b6505941c2814e01b779a7b58e952cf39680b706
mark leeds <markleeds2@gmail.com>
2015-04-25 16:48:55 -0400

    tried different aggregation methods

--- run_analysis.R
+++ run_analysis.R
@@ -13,8 +13,13 @@ if (!require("reshape2")) {
   install.packages("reshape2")
 }
 
+if (!require("tidyR")) {
+  install.packages("tidyr")
+}
+
 require("data.table")
 require("reshape2")
+require("tidyr")
 
 # LOAD ACTIVITY LABELS
 activity_labels <- read.table("UCI_HAR_Dataset/activity_labels.txt",stringsAsFactors=FALSE)[,2]
@@ -34,7 +39,6 @@ names(X_test) = features
 # CHANGE F TO FREQUENCY,
 # CHANGE MEAN() TO MEAN AND STD() TO STDDEV
 # REMOVE UNNECESSARY DASHES
-
 names(X_test) <- gsub("^f", "Frequency", names(X_test))
 names(X_test) <- gsub("^t", "Time", names(X_test))
 names(X_test) <- gsub("-mean\\(\\)", "Mean", names(X_test))
@@ -64,6 +68,7 @@ names(X_train) <- gsub("-", "", names(X_train))
 # CONSTRUCT REGULAR EXPRESSION THE MEASUREMENTS ON THE NON XYZ,
 # NON TIME RELATED, MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT.
 extract_features <- grepl("^f.*mean\\(\\)$|^f.*std\\(\\)$",features)
+print(features[which(extract_features)])
 
 # EXTRACT ONLY THE MEASUREMENTS ON THE MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT
 # IN BOTH X_TRAIN AND X_TEST
@@ -77,29 +82,69 @@ y_train[,2] = activity_labels[y_train[,1]]
 names(y_train) = c("Activity_ID", "Activity_Label")
 names(subject_train) = "subject"
 
-# BIND DATA USING FILTERED DATA
+# BIND DATA USING FILTERED TEST AND TRAIN DATA
 test_data <- data.frame(subject_test, y_test, X_test, stringsAsFactors=FALSE)
 train_data <- data.frame(subject_train, y_train, X_train, stringsAsFactors=FALSE)
 
-# MERGE TEST AND TRAIN DATA
-data = rbind(test_data, train_data)
+# MERGE FILTERED TEST AND TRAIN DATA
+compdata = rbind(test_data, train_data)
 
-id_labels   = c("subject", "Activity_Label","Activity_ID")
-data_labels = setdiff(colnames(data), id_labels)
+compdata$Activity_ID <- NULL
+
+id_labels   = c("subject", "Activity_Label")
+data_labels = setdiff(colnames(compdata), id_labels)
+
+print(head(id_labels))
+print(head(data_labels))
 
 # RESTRUCTURE INTO LONG FORM SO THAT AVERAGES CAN BE TAKEN OVER RESULTING FACTORS
-melt_data  = melt(data, id = id_labels, measure.vars = data_labels)
+melt_data  = melt(compdata, id = id_labels, measure.vars = data_labels)
 
-# NOW RUN DCAST TO GET AVERAGES
-tidy_data = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+# DO THE SAME RESTRUCTURING USING gather FROMM tidyr PACKAGE
+indices <- match(data_labels, names(compdata))
 
-print(head(tidy_data))
+gath_data <- gather(compdata, "variable", "value", indices)
 
-# ALSO TRY OTHER FOUR METHODS
-# LAPPLY, SPLIT
-# AGGREGATE
-# DPLYR
-# DATA.TABLE      
+#CONVERT FACTORS TO CHARACTER STRINGS
+gath_data <- data.frame(gath_data[c(1,2)], variable = as.character(gath_data$variable), value = gath_data$value,stringsAsFactors=FALSE)
+melt_data <- data.frame(melt_data[c(1,2)], variable = as.character(melt_data$variable), value = melt_data$value,stringsAsFactors=FALSE)
+identical(melt_data, gath_data)
+
+# A) NOW RUN DCAST TO GET SUBJECT, ACTIVITY MEANS
+tidy_cast = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+
+# B) DO IT USING LAPPLY(SPLIT)
+templist <- lapply(split(melt_data,list(melt_data$subject, melt_data$Activity_Label, melt_data$variable)), function(.df) {
+                   browser()
+                   datamatrix <- as.matrix(.df[,-1*c(1,2,3,4)])                   
+                   variable_means <- mean(datamatrix, na.rm = TRUE)
+                   avgDF <- data.frame(.df[1,c(1,2,3,4)], variable_means)
+                   avgDF                        
+                 })
+
+tidy_split <- do.call(rbind, templist)
+
+#C) DO THE SAME THING USING AGGREGATE
+agg_data <- melt_data
+agg_data$Activity_ID <- NULL
+tidy_agg <- aggregate( agg_data[4], agg_data[-4], mean)
+
+# D) DO THE SAME THING USING dplyr
+tidy_dplyr <- melt_data %>% group_by(subject, Activity_Label, Activity_ID, variable) %>% summarize(value = mean(value, na.rm = TRUE))
+
+tidy_dplyr <- as.data.frame(tidy_dplyr)
+
+# E)  DO THE SAME THING USING data.table
+dtbl <- data.table(melt_data)
+print(head(dtbl))
+tidy_dtbl <- dtbl[, mean(value), by = .(subject, Activity_Label,variable)]
+
+#=============================================
+print(head(tidy_agg))
+print(head(tidy_cast))
+print(head(tidy_split))
+print(head(tidy_dplyr))
+print(head(tidy_dtbl))
 
 write.table(tidy_data, file = "tidy_data.txt")
 
