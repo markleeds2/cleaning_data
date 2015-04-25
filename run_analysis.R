cbf0f36fad4f19437bd50388f9470e4fd66e0714
mark leeds <markleeds2@gmail.com>
2015-04-25 17:10:14 -0400

    decided on dplyr and gather. also created tidy_data.txt

--- run_analysis.R
+++ run_analysis.R
@@ -5,21 +5,16 @@
 ## 4. APPROPRIATELY LABELS THE DATA SET WITH DESCRIPTIVE ACTIVITY NAMES.
 ## 5. CREATES A SECOND, INDEPENDENT TIDY DATA SET WITH THE AVERAGE OF EACH VARIABLE FOR EACH ACTIVITY AND EACH SUBJECT.
 
-if (!require("data.table")) {
-  install.packages("data.table")
+if (!require("dplyr")) {
+  install.packages("dplyr")
 }
 
-if (!require("reshape2")) {
-  install.packages("reshape2")
-}
-
-if (!require("tidyR")) {
+if (!require("tidyr")) {
   install.packages("tidyr")
 }
 
-require("data.table")
-require("reshape2")
 require("tidyr")
+require("dplyr")
 
 # LOAD ACTIVITY LABELS
 activity_labels <- read.table("UCI_HAR_Dataset/activity_labels.txt",stringsAsFactors=FALSE)[,2]
@@ -68,7 +63,6 @@ names(X_train) <- gsub("-", "", names(X_train))
 # CONSTRUCT REGULAR EXPRESSION THE MEASUREMENTS ON THE NON XYZ,
 # NON TIME RELATED, MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT.
 extract_features <- grepl("^f.*mean\\(\\)$|^f.*std\\(\\)$",features)
-print(features[which(extract_features)])
 
 # EXTRACT ONLY THE MEASUREMENTS ON THE MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT
 # IN BOTH X_TRAIN AND X_TEST
@@ -89,64 +83,31 @@ train_data <- data.frame(subject_train, y_train, X_train, stringsAsFactors=FALSE
 # MERGE FILTERED TEST AND TRAIN DATA
 compdata = rbind(test_data, train_data)
 
+# NO LONGER NEED ACTIVITY_ID
 compdata$Activity_ID <- NULL
 
 id_labels   = c("subject", "Activity_Label")
 data_labels = setdiff(colnames(compdata), id_labels)
 
-print(head(id_labels))
-print(head(data_labels))
-
-# RESTRUCTURE INTO LONG FORM SO THAT AVERAGES CAN BE TAKEN OVER RESULTING FACTORS
-melt_data  = melt(compdata, id = id_labels, measure.vars = data_labels)
-
-# DO THE SAME RESTRUCTURING USING gather FROMM tidyr PACKAGE
+# USE gather FUNCTION FROM tidyr to CONVERT DATA TO LONG
 indices <- match(data_labels, names(compdata))
-
 gath_data <- gather(compdata, "variable", "value", indices)
 
-#CONVERT FACTORS TO CHARACTER STRINGS
-gath_data <- data.frame(gath_data[c(1,2)], variable = as.character(gath_data$variable), value = gath_data$value,stringsAsFactors=FALSE)
-melt_data <- data.frame(melt_data[c(1,2)], variable = as.character(melt_data$variable), value = melt_data$value,stringsAsFactors=FALSE)
-identical(melt_data, gath_data)
+# USING PLYR AND CHAINING TO GET MEANS BY DATA VARIABLES
+tidy_dplyr <- gath_data %>% group_by(subject, Activity_Label, variable) %>% summarize(value = mean(value, na.rm = TRUE))
 
-# A) NOW RUN DCAST TO GET SUBJECT, ACTIVITY MEANS
-tidy_cast = dcast(melt_data, subject + Activity_Label ~ variable, mean)
-
-# B) DO IT USING LAPPLY(SPLIT)
-templist <- lapply(split(melt_data,list(melt_data$subject, melt_data$Activity_Label, melt_data$variable)), function(.df) {
-                   browser()
-                   datamatrix <- as.matrix(.df[,-1*c(1,2,3,4)])                   
-                   variable_means <- mean(datamatrix, na.rm = TRUE)
-                   avgDF <- data.frame(.df[1,c(1,2,3,4)], variable_means)
-                   avgDF                        
-                 })
-
-tidy_split <- do.call(rbind, templist)
-
-#C) DO THE SAME THING USING AGGREGATE
-agg_data <- melt_data
-agg_data$Activity_ID <- NULL
-tidy_agg <- aggregate( agg_data[4], agg_data[-4], mean)
-
-# D) DO THE SAME THING USING dplyr
-tidy_dplyr <- melt_data %>% group_by(subject, Activity_Label, Activity_ID, variable) %>% summarize(value = mean(value, na.rm = TRUE))
-
+# CONVERT TO PLAIN OLD DATA.FRAME
 tidy_dplyr <- as.data.frame(tidy_dplyr)
 
-# E)  DO THE SAME THING USING data.table
-dtbl <- data.table(melt_data)
-print(head(dtbl))
-tidy_dtbl <- dtbl[, mean(value), by = .(subject, Activity_Label,variable)]
+# WRITE OUTPUT TO TEXT FILE
+write.table(tidy_dplyr, file = "tidy_data.txt")
 
-#=============================================
-print(head(tidy_agg))
-print(head(tidy_cast))
-print(head(tidy_split))
-print(head(tidy_dplyr))
-print(head(tidy_dtbl))
+#chk_data <- read.table("tidy_data.txt", sep = " ", header = TRUE)
+#print(head(chk_data))
 
-write.table(tidy_data, file = "tidy_data.txt")
+
+
+
 
 
 
